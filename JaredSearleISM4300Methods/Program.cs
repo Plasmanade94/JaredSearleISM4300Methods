bbb572b3625e954a02704cb054a914e8f8101b41
Jared Searle <jjsearle@usf.edu>
2021-02-05 11:33:04 -0500

    Add project files.

---
+++ JaredSearleISM4300Methods/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace JaredSearleISM4300Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name!");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name + "!");
+            Console.ReadKey(true);
+
+        }
+    }
+}
